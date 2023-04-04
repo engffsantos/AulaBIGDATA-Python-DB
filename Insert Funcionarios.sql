3a60d2de9808ce84c0450bd4a37b84ce3975980b
engffsantos <98486281+engffsantos@users.noreply.github.com>
2023-04-04 15:53:22 -0300

    Add files via upload

---
+++ Insert Funcionarios.sql
@@ -0,0 +1,18 @@
+INSERT INTO Funcionarios (PrimeiroNome, SegundoNome, UltimoNome, DataNasci, CPF, RG, Endereco, CEP, Cidade, Fone, CodigoDepartamento, Funcao, Salario)
+VALUES 
+    ('João', 'Silva', 'Santos', '1990-05-15', '123.456.789-10', '123456789', 'Rua A, 123', '12345-678', 'São Paulo', '(11) 98765-4321', 1, 'Analista de Sistemas', 5000.00),
+    ('Maria', 'Fernandes', 'Oliveira', '1985-02-10', '234.567.890-12', '234567890', 'Rua B, 456', '23456-789', 'Rio de Janeiro', '(21) 98765-4321', 2, 'Gerente de Vendas', 8000.00),
+    ('Pedro', 'Souza', 'Ferreira', '1995-09-20', '345.678.901-23', '345678901', 'Rua C, 789', '34567-890', 'Belo Horizonte', '(31) 98765-4321', 1, 'Desenvolvedor', 4000.00),
+    ('Ana', 'Santana', 'Barbosa', '1988-11-02', '456.789.012-34', '456789012', 'Rua D, 012', '45678-901', 'Curitiba', '(41) 98765-4321', 3, 'Analista de Marketing', 5500.00),
+    ('Lucas', 'Almeida', 'Castro', '1992-03-25', '567.890.123-45', '567890123', 'Rua E, 345', '56789-012', 'Recife', '(81) 98765-4321', 2, 'Vendedor', 3000.00),
+    ('Julia', 'Nascimento', 'Araújo', '1997-08-08', '678.901.234-56', '678901234', 'Rua F, 678', '67890-123', 'Porto Alegre', '(51) 98765-4321', 3, 'Analista de Recursos Humanos', 6000.00),
+    ('Rodrigo', 'Moraes', 'Ribeiro', '1983-01-30', '789.012.345-67', '789012345', 'Rua G, 901', '78901-234', 'Campinas', '(19) 98765-4321', 1, 'Arquiteto de Software', 7000.00),
+    ('Isabela', 'Oliveira', 'Pereira', '1987-06-12', '890.123.456-78', '890123456', 'Rua H, 234', '89012-345', 'Porto Alegre', '(51) 98765-4321', 2, 'Analista de Vendas', 4500.00),
+    ('Renato', 'da Silva', 'Costa', '1990-08-15', '123.456.789-10', '12.345.678-9', 'Rua das Flores, 123', '01234-567', 'São Paulo', '(11) 98765-4321', 3, 'Analista de Sistemas', 4500.00),
+    ('Fernanda', 'Almeida', 'Oliveira', '1985-04-27', '987.654.321-00', '98.765.432-1', 'Rua dos Ipês, 456', '09876-543', 'Belo Horizonte', '(31) 98765-4321', 2, 'Analista de Recursos Humanos', 5000.00),
+    ('André', 'dos Santos', 'Silva', '1988-10-22', '234.567.890-12', '23.456.789-0', 'Rua dos Carvalhos, 789', '89012-345', 'Curitiba', '(41) 98765-4321', 1, 'Gerente de Vendas', 7000.00),
+    ('Amanda', 'Garcia', 'Silveira', '1992-02-14', '345.678.901-23', '34.567.890-1', 'Rua dos Pinheiros, 321', '23456-789', 'Rio de Janeiro', '(21) 98765-4321', 2, 'Analista de Recursos Humanos', 5000.00),
+    ('Lucas', 'Barbosa', 'Silva', '1993-05-30', '456.789.012-34', '45.678.901-2', 'Avenida Paulista, 456', '34567-890', 'São Paulo', '(11) 98765-4321', 3, 'Analista de Sistemas', 4500.00),
+    ('Juliana', 'Silveira', 'Costa', '1991-11-05', '567.890.123-45', '56.789.012-3', 'Rua das Acácias, 678', '45678-901', 'Porto Alegre', '(51) 98765-4321', 1, 'Gerente de Vendas', 7000.00),
+    ('Felipe', 'Souza', 'Fernandes', '1989-08-20', '678.901.234-56', '67.890.123-4', 'Rua dos Lírios, 234', '56789-012', 'Florianópolis', '(48) 98765-4321', 2, 'Analista de Recursos Humanos', 5000.00);
+    
